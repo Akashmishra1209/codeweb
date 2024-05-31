af57720bcc194bdac09894ea11e6a55e756b6022
Akashmishra1209 <130904930+Akashmishra1209@users.noreply.github.com>
2024-05-31 14:19:55 +0530

    Discarded The Changes In Rich Text Editor

--- src/components/RichEditor.js
+++ src/components/RichEditor.js
@@ -15,18 +15,21 @@ export default function RichEditor() {
                 initialValue="<p>Welcome To WellText</p>"
                 init={{
                     height: 500,
-                    menubar: true,
+                    menubar: true, // Enable the menu bar
                     plugins: [
-                        'advlist autolink lists link image charmap preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table code help wordcount',
-                        'textcolor textpattern emoticons'
+                        // Essential plugins
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount',
+                        'textcolor', // Text color picker
+                        'textpattern', // Text patterns
+                        'emoticons', // Emoticons
                     ],
                     toolbar: 'undo redo | blocks | ' +
-                        'bold italic forecolor fontselect | alignleft aligncenter ' +
+                        'bold italic forecolor | alignleft aligncenter ' +
                         'alignright alignjustify | bullist numlist outdent indent | ' +
                         'removeformat | help',
-                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                    content_style: 'body {font - family:Helvetica,Arial,sans-serif; font-size:14px }'
                 }}
             />
         </>
