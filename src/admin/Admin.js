f054e05ccd25b481ed6ffded15e6a61fd9848ca5
Akashmishra1209 <130904930+Akashmishra1209@users.noreply.github.com>
2024-07-20 16:20:07 +0530

    Corrected Admin Routing

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,12 +1,11 @@
 // src/admin/Admin.js
-
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Dashboard from './Dashboard';
 import Users from './Users';
 import Settings from './Settings';
-import { isAuthenticated } from './auth'; // Assume you have an auth.js with this function
+import { isAuthenticated } from './auth';
 
 const Admin = () => {
     if (!isAuthenticated()) {
@@ -18,9 +17,9 @@ const Admin = () => {
             <Sidebar />
             <div className="flex-grow-1 p-3">
                 <Routes>
-                    <Route path="/admin/dashboard" element={<Dashboard />} />
-                    <Route path="/admin/users" element={<Users />} />
-                    <Route path="/admin/settings" element={<Settings />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="settings" element={<Settings />} />
                 </Routes>
             </div>
         </div>
